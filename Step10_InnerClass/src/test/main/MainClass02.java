03229ea2b22591f6688ee891081c9c742db7a125
eunjeong <eun646@naver.com>
2022-01-17 15:49:02 +0900

    클래스 안에 클래스를 넣을수도 있다..

---
+++ Step10_InnerClass/src/test/main/MainClass02.java
@@ -0,0 +1,13 @@
+package test.main;
+
+import test.mypac.MyUtil;
+import test.mypac.MyUtil.User;
+
+public class MainClass02 {
+
+	public static void main(String[] args) {
+		User u=new MyUtil().new User();
+
+	}
+
+}
