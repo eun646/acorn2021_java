c58d01ca0818b5d4dc54e0983fe47d9dc7dc7041
eunjeong <eun646@naver.com>
2022-01-18 10:11:44 +0900

    복습 겸 테스트

---
+++ Step11_AbstractClass/src/test/main/MainClass06.java
@@ -0,0 +1,22 @@
+package test.main;
+
+import test.mypac.MyWeapon;
+import test.mypac.Weapon;
+
+public class MainClass06 {
+
+	public static void main(String[] args) {
+		
+		Weapon w1=null;
+		
+		w1=new MyWeapon();
+		
+		w1.prepare();
+		w1.attack();
+			
+
+
+
+	}
+
+}
