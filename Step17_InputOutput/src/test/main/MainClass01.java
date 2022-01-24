41e678812e1f53940acea2f08d5f06325ada61b6
eunjeong <eun646@naver.com>
2022-01-24 09:54:23 +0900

    .

--- Step17_InputOutput/src/test/main/MainClass01.java
+++ Step17_InputOutput/src/test/main/MainClass01.java
@@ -2,6 +2,7 @@ package test.main;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainClass01 {
 	public static void main(String[] args) {
